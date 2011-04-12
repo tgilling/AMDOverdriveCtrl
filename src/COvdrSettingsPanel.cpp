fd7962a16b0e64c96a2b595f119116ee1934269c
tgilling <tgilling@web.de>
2011-04-12 11:43:05 +0200

    code format (ident 4, tab 8)

--- src/COvdrSettingsPanel.cpp
+++ src/COvdrSettingsPanel.cpp
@@ -39,27 +39,27 @@ COvdrSettingsPanel::COvdrSettingsPanel(wxWindow* parent, wxWindowID id, const wx
 {
     adl = ADL::Instance();
 
-    if (!(adl->GetSupportedFeatures() & ADL::FEAT_GET_OD_PARAMETERS) ||
-	!(adl->GetSupportedFeatures() & ADL::FEAT_GET_OD_PERF_LEVELS))
+    if(!(adl->GetSupportedFeatures() & ADL::FEAT_GET_OD_PARAMETERS) ||
+            !(adl->GetSupportedFeatures() & ADL::FEAT_GET_OD_PERF_LEVELS))
     {
-	Show(false);
+        Show(false);
     }
     else
     {
-	mpSettingsPanelLow = new CSettingsPanel(0, mOvdrNotebook);
-	mpSettingsPanelMid = new CSettingsPanel(1, mOvdrNotebook);
-	mpSettingsPanelHigh = new CSettingsPanel(2, mOvdrNotebook);
+        mpSettingsPanelLow = new CSettingsPanel(0, mOvdrNotebook);
+        mpSettingsPanelMid = new CSettingsPanel(1, mOvdrNotebook);
+        mpSettingsPanelHigh = new CSettingsPanel(2, mOvdrNotebook);
 
-	if(mpSettingsPanelLow != NULL && mpSettingsPanelMid != NULL && mpSettingsPanelHigh != NULL)
-	{
-	    mOvdrNotebook->AddPage(mpSettingsPanelLow, wxT("Low"));
-	    mOvdrNotebook->AddPage(mpSettingsPanelMid, wxT("Mid"));
-	    mOvdrNotebook->AddPage(mpSettingsPanelHigh, wxT("High"));
-	}
-	else
-	{
-	    wxMessageBox(wxT("Unable to create Panels"), wxT("ERROR"), wxOK|wxCENTRE|wxICON_ERROR);
-	}
+        if(mpSettingsPanelLow != NULL && mpSettingsPanelMid != NULL && mpSettingsPanelHigh != NULL)
+        {
+            mOvdrNotebook->AddPage(mpSettingsPanelLow, wxT("Low"));
+            mOvdrNotebook->AddPage(mpSettingsPanelMid, wxT("Mid"));
+            mOvdrNotebook->AddPage(mpSettingsPanelHigh, wxT("High"));
+        }
+        else
+        {
+            wxMessageBox(wxT("Unable to create Panels"), wxT("ERROR"), wxOK|wxCENTRE|wxICON_ERROR);
+        }
     }
 }
 
@@ -70,71 +70,71 @@ COvdrSettingsPanel::~COvdrSettingsPanel()
 
 void COvdrSettingsPanel::SetOverdriveValues(int PerfLevel, int gpu, int mem, int volt)
 {
-    if (PerfLevel >= 0 && PerfLevel < NR_OF_LEVELS)
+    if(PerfLevel >= 0 && PerfLevel < NR_OF_LEVELS)
     {
-	mGPU[PerfLevel] = gpu;
-	mMem[PerfLevel] = mem;
-	mVoltage[PerfLevel] = volt;
+        mGPU[PerfLevel] = gpu;
+        mMem[PerfLevel] = mem;
+        mVoltage[PerfLevel] = volt;
     }
 }
 
 bool COvdrSettingsPanel::CommitOverdriveValues()
 {
-    if ((adl->GetSupportedFeatures() & ADL::FEAT_GET_OD_PARAMETERS) && (adl->GetSupportedFeatures() & ADL::FEAT_GET_OD_PERF_LEVELS))
+    if((adl->GetSupportedFeatures() & ADL::FEAT_GET_OD_PARAMETERS) && (adl->GetSupportedFeatures() & ADL::FEAT_GET_OD_PERF_LEVELS))
     {
-    #ifdef FAKE_ATI_CARD
-	for (int i=0; i<NR_OF_LEVELS; i++)
-	{
-	    ACT_LOG("SetOverdriveValues: Level " i << " " << mGPU[i] << "MHz, " << mMem[i] << "MHz, " <<	"MHz, " << (double)mVoltage[i]/1000.0 << "V");
-	}
-	return true;
-    #endif
+#ifdef FAKE_ATI_CARD
+        for(int i=0; i<NR_OF_LEVELS; i++)
+        {
+            ACT_LOG("SetOverdriveValues: Level " i << " " << mGPU[i] << "MHz, " << mMem[i] << "MHz, " <<	"MHz, " << (double)mVoltage[i]/1000.0 << "V");
+        }
+        return true;
+#endif
 
-	ADLODParameters para;
-	ADLODPerformanceLevels* levels;
+        ADLODParameters para;
+        ADLODPerformanceLevels* levels;
 
-	ADL* adl = ADL::Instance();
+        ADL* adl = ADL::Instance();
 
-	if(SAVE_CALL(adl->ADL_Overdrive5_ODParameters_Get)(adl->GetGPUIndex(), &para) == ADL_OK)
-	{
-	    int perf_level_size = sizeof(ADLODPerformanceLevels) + sizeof(ADLODPerformanceLevel) * (para.iNumberOfPerformanceLevels - 1);
-	    levels = (ADLODPerformanceLevels*)malloc(perf_level_size);
-	    levels->iSize = perf_level_size;
+        if(SAVE_CALL(adl->ADL_Overdrive5_ODParameters_Get)(adl->GetGPUIndex(), &para) == ADL_OK)
+        {
+            int perf_level_size = sizeof(ADLODPerformanceLevels) + sizeof(ADLODPerformanceLevel) * (para.iNumberOfPerformanceLevels - 1);
+            levels = (ADLODPerformanceLevels*)malloc(perf_level_size);
+            levels->iSize = perf_level_size;
 
-	    if(SAVE_CALL(adl->ADL_Overdrive5_ODPerformanceLevels_Get)(adl->GetGPUIndex(), 0, levels) == ADL_OK)
-	    {
-		for (int i=0; i<NR_OF_LEVELS; i++)
-		{
-		    levels->aLevels[i].iEngineClock = mGPU[i] * 100;
-		    levels->aLevels[i].iMemoryClock = mMem[i] * 100;
-		    levels->aLevels[i].iVddc = mVoltage[i];
+            if(SAVE_CALL(adl->ADL_Overdrive5_ODPerformanceLevels_Get)(adl->GetGPUIndex(), 0, levels) == ADL_OK)
+            {
+                for(int i=0; i<NR_OF_LEVELS; i++)
+                {
+                    levels->aLevels[i].iEngineClock = mGPU[i] * 100;
+                    levels->aLevels[i].iMemoryClock = mMem[i] * 100;
+                    levels->aLevels[i].iVddc = mVoltage[i];
 
-		    ACT_LOG("SetOverdriveValues: Level " << i << ": " << mGPU[i] << "MHz, " << mMem[i] << "MHz, " <<	"MHz, " << (double)mVoltage[i]/1000.0 << "V");
-		}
-	    }
+                    ACT_LOG("SetOverdriveValues: Level " << i << ": " << mGPU[i] << "MHz, " << mMem[i] << "MHz, " <<	"MHz, " << (double)mVoltage[i]/1000.0 << "V");
+                }
+            }
 
-	    if(SAVE_CALL(adl->ADL_Overdrive5_ODPerformanceLevels_Set)(adl->GetGPUIndex(), levels) != ADL_OK)
-	    {
-		return false;
-	    }
+            if(SAVE_CALL(adl->ADL_Overdrive5_ODPerformanceLevels_Set)(adl->GetGPUIndex(), levels) != ADL_OK)
+            {
+                return false;
+            }
 
-	    free(levels);
-	    levels = NULL;
+            free(levels);
+            levels = NULL;
 
-	    return true;
-	}
-	return false;
+            return true;
+        }
+        return false;
     }
     return true;
 }
 
 void COvdrSettingsPanel::UpdateDisplayValues()
 {
-    if (mpSettingsPanelLow != NULL && mpSettingsPanelMid != NULL && mpSettingsPanelHigh != NULL)
+    if(mpSettingsPanelLow != NULL && mpSettingsPanelMid != NULL && mpSettingsPanelHigh != NULL)
     {
-	mpSettingsPanelLow->UpdateDisplayValues();
-	mpSettingsPanelMid->UpdateDisplayValues();
-	mpSettingsPanelHigh->UpdateDisplayValues();
+        mpSettingsPanelLow->UpdateDisplayValues();
+        mpSettingsPanelMid->UpdateDisplayValues();
+        mpSettingsPanelHigh->UpdateDisplayValues();
     }
 }
 
@@ -170,55 +170,55 @@ void COvdrSettingsPanel::mButtonSetClick(wxCommandEvent& WXUNUSED(event))
     mpSettingsPanelHigh->GetValues(level2, gpu2, mem2, v2);
 
     wxString tmp = wxString::FromAscii(adl->mpAdapterInfo->strAdapterName);
-    if (tmp.Find(wxT("Mobility")) == wxNOT_FOUND)
+    if(tmp.Find(wxT("Mobility")) == wxNOT_FOUND)
     {
-	// mobility chips seem to be OK, maybe only the HD48xx series has a problem, I don't know... so warn if desktop HW is detected
-	if (mem0 != mem1 || mem1 != mem2)
-	{
-	    if (wxMessageBox(wxT("You have choosen different memory clock settings\nfor the three performance levels.\n\n")
-			     wxT("This may result in screen flickering on certain hardware.\n\n")
-			     wxT("Are you sure you want to set these values?"), wxT("Are you sure?"), wxYES_NO|wxCENTRE|wxICON_WARNING) == wxNO)
-	    {
-		return;
-	    }
-	}
+        // mobility chips seem to be OK, maybe only the HD48xx series has a problem, I don't know... so warn if desktop HW is detected
+        if(mem0 != mem1 || mem1 != mem2)
+        {
+            if(wxMessageBox(wxT("You have choosen different memory clock settings\nfor the three performance levels.\n\n")
+                            wxT("This may result in screen flickering on certain hardware.\n\n")
+                            wxT("Are you sure you want to set these values?"), wxT("Are you sure?"), wxYES_NO|wxCENTRE|wxICON_WARNING) == wxNO)
+            {
+                return;
+            }
+        }
     }
 
-    if (gpu0 <= gpu1 && gpu1 <= gpu2 && mem0 <= mem1 && mem1 <= mem2)
+    if(gpu0 <= gpu1 && gpu1 <= gpu2 && mem0 <= mem1 && mem1 <= mem2)
     {
-	SetOverdriveValues(level0, gpu0, mem0, v0);
-	SetOverdriveValues(level1, gpu1, mem1, v1);
-	SetOverdriveValues(level2, gpu2, mem2, v2);
+        SetOverdriveValues(level0, gpu0, mem0, v0);
+        SetOverdriveValues(level1, gpu1, mem1, v1);
+        SetOverdriveValues(level2, gpu2, mem2, v2);
 
-	CommitOverdriveValues();
+        CommitOverdriveValues();
     }
     else
     {
-	wxMessageBox(wxT("\nThe choosen overdrive settings are not valid.\n\nFrequency settings must follow the rule:\n\n"
-	 "low level <= mid level <= high level"), wxT("Invalid settings"), wxOK|wxCENTRE|wxICON_ERROR);
+        wxMessageBox(wxT("\nThe choosen overdrive settings are not valid.\n\nFrequency settings must follow the rule:\n\n"
+                         "low level <= mid level <= high level"), wxT("Invalid settings"), wxOK|wxCENTRE|wxICON_ERROR);
     }
 }
 void COvdrSettingsPanel::mButtonSaveDefaultClick(wxCommandEvent& WXUNUSED(event))
 {
-	if(wxMessageBox(wxT("Saving these overdrive settings will override any previously saved default settings in ~/.AMDOverDriveCtrl/default.ovdr.\n\n")
-				wxT("Override default settings?"), wxT("Override default settings?"), wxYES_NO|wxICON_QUESTION) == wxYES) 
-	{
-		// Call mButtonSetClick to update Overdrive values
-		wxCommandEvent event;
-		this->mButtonSetClick(event);
-		
-		if(getenv("HOME"))
-		{
-			
-			// Get path of default.ovdr
-			wxString file_path = wxString::FromAscii(getenv("HOME"));
-			file_path += wxT("/.AMDOverdriveCtrl/default.ovdr");
-			
-			MainDialog* mMD;
-			mMD = (MainDialog*)this->GetParent()->GetParent(); // get a pointer to the MainDialog object
-			mMD->SaveXML(file_path); // reuse MainDialog::SaveXML
-			
-			INF_LOG("mButtonSaveDefaultClick: saved " << file_path.mb_str(wxConvUTF8));
-		}
-	}
+    if(wxMessageBox(wxT("Saving these overdrive settings will override any previously saved default settings in ~/.AMDOverDriveCtrl/default.ovdr.\n\n")
+                    wxT("Override default settings?"), wxT("Override default settings?"), wxYES_NO|wxICON_QUESTION) == wxYES)
+    {
+        // Call mButtonSetClick to update Overdrive values
+        wxCommandEvent event;
+        this->mButtonSetClick(event);
+
+        if(getenv("HOME"))
+        {
+
+            // Get path of default.ovdr
+            wxString file_path = wxString::FromAscii(getenv("HOME"));
+            file_path += wxT("/.AMDOverdriveCtrl/default.ovdr");
+
+            MainDialog* mMD;
+            mMD = (MainDialog*)this->GetParent()->GetParent(); // get a pointer to the MainDialog object
+            mMD->SaveXML(file_path); // reuse MainDialog::SaveXML
+
+            INF_LOG("mButtonSaveDefaultClick: saved " << file_path.mb_str(wxConvUTF8));
+        }
+    }
 }
